cc775ce8f9b75b92155c39ebc242655b036f5a94
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2025-09-29 23:29:26 +0900

    feat: Added an example for global application load balancer with IAP

---
+++ lb-l7-iap/terraform/service.tf
@@ -0,0 +1,60 @@
+resource "google_project" "main" {
+  name            = "${var.project_id_prefix}-service"
+  project_id      = "${var.project_id_prefix}-service"
+  billing_account = data.google_billing_account.billing.id
+  folder_id       = var.folder_id
+
+  # TODO
+  deletion_policy = "DELETE"
+}
+
+# Timeout
+resource "time_sleep" "wait_for_project" {
+  create_duration = "60s"
+  depends_on      = [google_project.main]
+}
+
+# api
+locals {
+  apis = [
+    "run.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "main" {
+  for_each = toset(local.apis)
+
+  project            = google_project.main.project_id
+  service            = each.key
+  disable_on_destroy = false
+
+  depends_on = [time_sleep.wait_for_project]
+}
+
+resource "google_service_account" "run" {
+  project      = google_project.main.project_id
+  account_id   = "${var.project_id_prefix}-run"
+  display_name = "${var.project_id_prefix}-run"
+  description  = "Service Account for ${var.project_id_prefix}-run"
+}
+
+resource "google_cloud_run_v2_service" "main" {
+  project  = google_project.main.project_id
+  location = var.region
+  name     = "${var.project_id_prefix}-run"
+
+  template {
+    service_account = google_service_account.run.email
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello:latest"
+    }
+  }
+
+  ingress = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
+
+  invoker_iam_disabled = true
+
+  # TODO
+  deletion_protection = false
+  depends_on          = [google_project_service.main]
+}
